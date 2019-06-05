16c53c8d949621fe1d8da6002f2f97bf18f8bba2
tiger <barney211@163.com>
2019-06-05 16:31:26 +0800

    Initial commit

---
+++ pages/picShow/picShow.js
@@ -0,0 +1,84 @@
+// pages/picShow/picShow.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      productList:[
+          {title:'海信4k',extra:'32寸',detail:'海信4k,32寸电视'},
+          {title:'海信智能',extra:'Android',detail:'海信4k,32寸电视'},
+          {title:'海信4k',extra:'32寸',detail:'海信4k,32寸电视'},
+          {title:'海信智能',extra:'Android',detail:'海信4k,32寸电视'},
+          {title:'海信4k',extra:'32寸',detail:'海信4k,32寸电视'},
+          {title:'海信智能',extra:'Android',detail:'海信4k,32寸电视'},
+          {title:'海信4k',extra:'32寸',detail:'海信4k,32寸电视'},
+          {title:'海信智能',extra:'Android',detail:'海信4k,32寸电视'}
+      ]
+  },
+    showInfo(){
+        this.setData({
+            visible2: true
+        });
+    },
+    handleClose2 () {
+        this.setData({
+            visible2: false
+        });
+    },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
